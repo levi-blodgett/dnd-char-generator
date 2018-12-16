162957a4dbb22ba514f9b2c403cb61694aa3ca9b
Levi <levi_blodgett@outlook.com>
2018-12-15 22:16:18 -0600

    Added styling to the top buttons, might change them

---
+++ js/styling.js
@@ -0,0 +1,20 @@
+const UI_topButtons = document.querySelectorAll('.top_buttons');
+
+function giveStyling(list){
+  for (let i = 0; i < list.length; i++) {
+    list[i].addEventListener("mousedown", function(){
+      list[i].style.background = "#c0c0c0";
+    })
+    list[i].addEventListener("mouseup", function(){
+      list[i].style.background = "white";
+    })
+    list[i].addEventListener("mouseover", function(){
+      list[i].style.background = "#e6e6e6";
+    })
+    list[i].addEventListener("mouseout", function(){
+      list[i].style.background = "white";
+    })
+  }
+}
+
+giveStyling(UI_topButtons);
