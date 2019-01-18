54a102619614bb2f7b9de09734abfd8a135cd411
Levi <levi_blodgett@outlook.com>
2019-01-17 21:36:59 -0600

    Cut down dropdowns.js further, more organized and condensed

--- js/dropdowns.js
+++ js/dropdowns.js
@@ -13,77 +13,6 @@ function formatInitialDropdowns(list, listItems) {
     }
   }
 }
-let checker = 0;
-let raceRandom = document.getElementById('race_random').checked;
-if (raceRandom === '' || raceRandom === undefined){
-  for(i = 0; i < raceClass.length; i++){
-    if (raceClass[i].checked === 'checked'){
-      raceRandom = '';
-      checker = checker + 1;
-    }
-  }
-  if (checker > 0){
-      raceRandom = '';
-    } else {
-      raceRandom = 'checked';
-    }
-}
-
-function hideDropdowns(dropdownValue){
-  dropdownValue.classList.remove('visible');
-  dropdownValue.style.display = "none";
-}
-
-function onClickDecider(_id, _class){
-  let checkbox_checked = document.getElementById(_id).checked;
-
-  let classer = _class;
-  let ID = _id;
-
-  let lengthOfArray;
-  let arraything;
-  
-  let checkerarray;
-  if (checkbox_checked){
-    lengthOfArray = document.querySelectorAll(`input.${classer}`).length;
-    arraything = document.querySelectorAll(`input.${classer}`);
-    for(i=0; i < lengthOfArray; i++){
-      arraything[i].checked = false;
-    }
-    if (classer === 'race_class'){
-      let lengthOfArray2 = document.querySelectorAll(`input.${classer}_2`).length;
-      let arraything2 = document.querySelectorAll(`input.${classer}_2`);
-      let dropdown1 = document.getElementById('dragonborn_dropdown');
-      let dropdown2 = document.getElementById('dwarf_dropdown');
-      let dropdown3 = document.getElementById('elf_dropdown');
-      let dropdown4 = document.getElementById('gnome_dropdown');
-      let dropdown5 = document.getElementById('halfling_dropdown');
-      let dropdown6 = document.getElementById('human_dropdown');
-      let checkerarray2= [];
-      hideDropdowns(dropdown1);
-      hideDropdowns(dropdown2);
-      hideDropdowns(dropdown3);
-      hideDropdowns(dropdown4);
-      hideDropdowns(dropdown5);
-      hideDropdowns(dropdown6);
-      for(i=0; i < lengthOfArray2; i++){
-        arraything2[i].checked = false;
-      }
-    }
-  } else if (checkbox_checked === false){ 
-    lengthOfArray = document.querySelectorAll(`input.${classer}`).length;
-    arraything = document.querySelectorAll(`input.${classer}`);
-    checkerarray= [];
-    for(i=0; i < lengthOfArray; i++){
-      if (arraything[i].checked === true) {
-        checkerarray.push(arraything[i]);
-      }
-    }
-    if (checkerarray.length === 0){
-      document.getElementById(ID).checked = true;
-    }
-  }
-}
 
 function formatSecondaryDropdowns(list, listItems, checkbox){
   let checkList = document.getElementById(list);
@@ -112,8 +41,56 @@ function formatSecondaryDropdowns(list, listItems, checkbox){
   });
 }
 
+function hideDropdowns(dropdownValue){
+  dropdownValue.classList.remove('visible');
+  dropdownValue.style.display = "none";
+}
+
+function onClickDecider(_id, _class){
+  let checkbox_checked = document.getElementById(_id).checked;
+
+  let classer = _class;
+  let ID = _id;
+  let lengthOfArray;
+  let arrayItself;
+  
+  let checkerarray;
+  if (checkbox_checked){
+    lengthOfArray = document.querySelectorAll(`input.${classer}`).length;
+    arrayItself = document.querySelectorAll(`input.${classer}`);
+    for(i=0; i < lengthOfArray; i++){
+      arrayItself[i].checked = false;
+    }
+    if (classer === 'race_class'){
+      let lengthOfArray2 = document.querySelectorAll(`input.${classer}_2`).length;
+      let arrayItself2 = document.querySelectorAll(`input.${classer}_2`);
+      let dropdowns = ['dragonborn_dropdown', 'dwarf_dropdown', 'elf_dropdown', 'gnome_dropdown', 'halfling_dropdown', 'human_dropdown']
+      let dropdown;
+      for (i = 0; i < formatInitialDropdowns.length; i++) {
+        dropdown = document.getElementById(dropdowns[i]);
+        hideDropdowns(dropdown);
+      }
+      for(i=0; i < lengthOfArray2; i++){
+        arrayItself2[i].checked = false;
+      }
+    }
+  } else if (checkbox_checked === false){ 
+    lengthOfArray = document.querySelectorAll(`input.${classer}`).length;
+    arrayItself = document.querySelectorAll(`input.${classer}`);
+    checkerarray= [];
+    for(i=0; i < lengthOfArray; i++){
+      if (arrayItself[i].checked === true) {
+        checkerarray.push(arrayItself[i]);
+      }
+    }
+    if (checkerarray.length === 0){
+      document.getElementById(ID).checked = true;
+    }
+  }
+}
+
 // Event listener function to check if the random race checkmark is checked
-function onSubraceDropdownOptionClick_mousedown(e){
+function onSubraceDropdownOptionClick(e){
   let targets_ul = document.getElementById(e.target.parentElement.parentElement.id);
   let targets_list = targets_ul.children;
   let targets_list_array = Array.from(targets_list);
@@ -134,15 +111,16 @@ function onSubraceDropdownOptionClick_mousedown(e){
   }
 }
 
-let all_subrace_dropdowns_down = document.querySelectorAll('.subrace_dropdown_option_checker');
-for(i=0; i < all_subrace_dropdowns_down.length; i++){
-  all_subrace_dropdowns_down[i].addEventListener('mousedown', onSubraceDropdownOptionClick_mousedown, false);
-}
-
-const dropdownArrayGetter = (dropdown_option_checker, _id, _class) => {
+const dropdownArrayGetter = (dropdown_option_checker, _id, _class, mouseAction, _function) => {
   let all_dropdowns = document.querySelectorAll(dropdown_option_checker);
   for(i=0; i < all_dropdowns.length; i++){
-    all_dropdowns[i].addEventListener('click', () => {onDropdownOptionClick(_id, _class)});
+    all_dropdowns[i].addEventListener(mouseAction, (e) => {
+      if (_function === onSubraceDropdownOptionClick) {
+        _function(e);
+      } else {
+        _function(_id, _class);
+      }
+    });
   }
 }
 
@@ -151,16 +129,16 @@ function checkRandom() {
   let classer = 'race_class_2';
   let ID = 'race_random';
   let lengthOfArray;
-  let arraything;
+  let arrayItself;
   let checkerarray;
 
   if (checkbox_checked === false){
     lengthOfArray = document.querySelectorAll(`input.${classer}`).length;
-    arraything = document.querySelectorAll(`input.${classer}`);
+    arrayItself = document.querySelectorAll(`input.${classer}`);
     checkerarray= [];
     for(i=0; i < lengthOfArray; i++){
-      if (arraything[i].checked === true) {
-        checkerarray.push(arraything[i]);
+      if (arrayItself[i].checked === true) {
+        checkerarray.push(arrayItself[i]);
       }
     }
     if (checkerarray.length === 0){
@@ -178,11 +156,11 @@ function onDropdownOptionClick(_id, _class){
     document.getElementById(ID_1).checked = false;
   } else if (checkbox_checked_1 === false){ 
     lengthOfArray = document.querySelectorAll(`input.${racer_2}`).length;
-    arraything = document.querySelectorAll(`input.${racer_2}`);
+    arrayItself = document.querySelectorAll(`input.${racer_2}`);
     checkerarray= [];
     for(i=0; i < lengthOfArray; i++){
-      if (arraything[i].checked === true) {
-        checkerarray.push(arraything[i]);
+      if (arrayItself[i].checked === true) {
+        checkerarray.push(arrayItself[i]);
       }
     }
     if (checkerarray.length === 0){
@@ -190,26 +168,6 @@ function onDropdownOptionClick(_id, _class){
     }
   }
 }
-
-formatInitialDropdowns('race_list', 'race_dropdown');
-formatInitialDropdowns('background_list', 'background_dropdown');
-formatInitialDropdowns('class_list', 'class_dropdown', 'class_class');
-formatInitialDropdowns('alignment_list', 'alignment_dropdown');
-
-let all_alignment_dropdowns = dropdownArrayGetter('.alignment_dropdown_option_checker', 'alignment_random', 'alignment_class');
-
-let all_class_dropdowns = dropdownArrayGetter('.class_dropdown_option_checker', 'class_random', 'class_class');
-
-let all_background_dropdowns = dropdownArrayGetter('.background_dropdown_option_checker', 'background_random', 'background_class');
-
-let all_race_dropdowns = dropdownArrayGetter('.race_dropdown_option_checker', 'race_random', 'race_class');
-
-formatSecondaryDropdowns('dragonborn_list', 'dragonborn_dropdown', 'dragonborn_checkbox');
-formatSecondaryDropdowns('dwarf_list', 'dwarf_dropdown', 'dwarf_checkbox');
-formatSecondaryDropdowns('elf_list', 'elf_dropdown', 'elf_checkbox');
-formatSecondaryDropdowns('gnome_list', 'gnome_dropdown', 'gnome_checkbox');
-formatSecondaryDropdowns('halfling_list', 'halfling_dropdown', 'halfling_checkbox');
-formatSecondaryDropdowns('human_list', 'human_dropdown', 'human_checkbox');
 
 document.getElementById("race_random").addEventListener('click', function() {
   onClickDecider('race_random', 'race_class');
@@ -226,3 +184,19 @@ document.getElementById("class_random").addEventListener('click', function(){
 document.getElementById("alignment_random").addEventListener('click', function(){
   onClickDecider('alignment_random', 'alignment_class');
 });
+
+formatInitialDropdowns('race_list', 'race_dropdown');
+formatInitialDropdowns('background_list', 'background_dropdown');
+formatInitialDropdowns('class_list', 'class_dropdown', 'class_class');
+formatInitialDropdowns('alignment_list', 'alignment_dropdown');
+formatSecondaryDropdowns('dragonborn_list', 'dragonborn_dropdown', 'dragonborn_checkbox');
+formatSecondaryDropdowns('dwarf_list', 'dwarf_dropdown', 'dwarf_checkbox');
+formatSecondaryDropdowns('elf_list', 'elf_dropdown', 'elf_checkbox');
+formatSecondaryDropdowns('gnome_list', 'gnome_dropdown', 'gnome_checkbox');
+formatSecondaryDropdowns('halfling_list', 'halfling_dropdown', 'halfling_checkbox');
+formatSecondaryDropdowns('human_list', 'human_dropdown', 'human_checkbox');
+dropdownArrayGetter('.alignment_dropdown_option_checker', 'alignment_random', 'alignment_class', 'click', onDropdownOptionClick);
+dropdownArrayGetter('.class_dropdown_option_checker', 'class_random', 'class_class', 'click', onDropdownOptionClick);
+dropdownArrayGetter('.background_dropdown_option_checker', 'background_random', 'background_class', 'click', onDropdownOptionClick);
+dropdownArrayGetter('.race_dropdown_option_checker', 'race_random', 'race_class', 'click', onDropdownOptionClick);
+dropdownArrayGetter('.subrace_dropdown_option_checker', 'race_random', 'race_class', 'mousedown', onSubraceDropdownOptionClick);
