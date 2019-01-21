60545141c602be492b0604796ddf76286ae7bff9
Levi <levi_blodgett@outlook.com>
2019-01-20 20:18:28 -0600

    Changed variable and function names

--- js/dropdowns.js
+++ js/dropdowns.js
@@ -1,5 +1,5 @@
 // Format and style the initial dropdowns
-function formatInitialDropdowns(list, listItems) {
+function format_initial_dropdowns(list, listItems) {
   let checkList = document.getElementById(list);
   let items = document.getElementById(listItems);
   checkList.getElementsByClassName('anchor')[0].onclick = () => {
@@ -15,25 +15,25 @@ function formatInitialDropdowns(list, listItems) {
 }
 
 // Format and style the initial dropdowns inside of the race dropdown
-function formatSecondaryDropdowns(list, listItems, checkbox){
+function format_secondary_dropdowns(list, listItems, checkbox){
   let checkList = document.getElementById(list);
   let items = document.getElementById(listItems);
-  let checkerino = document.getElementById(checkbox);
-  let the_checker = document.querySelectorAll(`ul#${listItems} input.race_class`);
+  let raceCheckbox = document.getElementById(checkbox);
+  let listOfSubracesInDropdown = document.querySelectorAll(`ul#${listItems} input.race_class`);
   checkList.getElementsByClassName('chain')[0].addEventListener('click', function() {
       if (items.classList.contains('visible')){
           items.classList.remove('visible');
           items.style.display = "none";
-          checkerino.checked = false;
-          for(i = 0; i < the_checker.length; i++){
-            the_checker[i].checked = false;
+          raceCheckbox.checked = false;
+          for(i = 0; i < listOfSubracesInDropdown.length; i++){
+            listOfSubracesInDropdown[i].checked = false;
           }
       } else{
           items.classList.add('visible');
           items.style.display = "block";
-          checkerino.checked = true;
-          for(i = 0; i < the_checker.length; i++){
-            the_checker[i].checked = true;
+          raceCheckbox.checked = true;
+          for(i = 0; i < listOfSubracesInDropdown.length; i++){
+            listOfSubracesInDropdown[i].checked = true;
           }
       }
   }, true);
@@ -43,13 +43,13 @@ function formatSecondaryDropdowns(list, listItems, checkbox){
 }
 
 // Hide any dropdowns passed in this ()
-function hideDropdowns(dropdownValue){
+function hide_dropdowns(dropdownValue){
   dropdownValue.classList.remove('visible');
   dropdownValue.style.display = "none";
 }
 
 // This () decides whether or not to hide any dropdowns passed through
-function onClickDecider(_id, _class){
+function on_click_decider(_id, _class){
   let isCheckboxChecked = document.getElementById(_id).checked;
   let input_class = _class;
   let ID = _id;
@@ -67,9 +67,9 @@ function onClickDecider(_id, _class){
       let arrayItself_2 = document.querySelectorAll(`input.${input_class}_2`);
       let dropdownIDs = ['dragonborn_dropdown', 'dwarf_dropdown', 'elf_dropdown', 'gnome_dropdown', 'halfling_dropdown', 'human_dropdown']
       let dropdown;
-      for (i = 0; i < formatInitialDropdowns.length; i++) {
+      for (i = 0; i < format_initial_dropdowns.length; i++) {
         dropdown = document.getElementById(dropdownIDs[i]);
-        hideDropdowns(dropdown);
+        hide_dropdowns(dropdown);
       }
       for(i=0; i < lengthOfArray_2; i++){
         arrayItself_2[i].checked = false;
@@ -91,7 +91,7 @@ function onClickDecider(_id, _class){
 }
 
 // () created for an event listener click, to decide whether or not to hide the secondary dropdown parent element that is being clicked and click random option if no subrace is clicked
-function onSubraceDropdownOptionClick(e){
+function on_subrace_dropdown_option_click(e){
   let targets_ul = document.getElementById(e.target.parentElement.parentElement.id);
   let targets_list = targets_ul.children;
   let targets_list_array = Array.from(targets_list);
@@ -105,7 +105,7 @@ function onSubraceDropdownOptionClick(e){
           e.target.parentElement.parentElement.classList.remove('visible');
           e.target.parentElement.parentElement.previousElementSibling.childNodes[0].childNodes[0].checked = false;
           e.target.childNodes[0].checked = false;
-          onDropdownOptionClick('race_random', 'race_class_2');
+          on_dropdown_option_click('race_random', 'race_class_2');
         }
       }
     }
@@ -113,11 +113,11 @@ function onSubraceDropdownOptionClick(e){
 }
 
 // () created for adding an event listener based on what is passed through
-const addEventsForDropdowns = (dropdown_option_checker, _id, _class, mouseAction, _function) => {
+const add_events_for_dropdowns = (dropdown_option_checker, _id, _class, mouseAction, _function) => {
   let all_dropdowns = document.querySelectorAll(dropdown_option_checker);
   for(i=0; i < all_dropdowns.length; i++){
     all_dropdowns[i].addEventListener(mouseAction, (e) => {
-      if (_function === onSubraceDropdownOptionClick) {
+      if (_function === on_subrace_dropdown_option_click) {
         _function(e);
       } else {
         _function(_id, _class);
@@ -127,7 +127,7 @@ const addEventsForDropdowns = (dropdown_option_checker, _id, _class, mouseAction
 }
 
 // Event listener function to check if the random checkmark is checked
-function onDropdownOptionClick(_id, _class){
+function on_dropdown_option_click(_id, _class){
   let ID = _id;
   let input_class = _class;
   let isCheckboxChecked_1 = document.getElementById(ID).checked;
@@ -150,35 +150,35 @@ function onDropdownOptionClick(_id, _class){
 
 // Section that adds click event listeners to the random buttons to see if they'll hide and uncheck all other buttons
 document.getElementById("race_random").addEventListener('click', function() {
-  onClickDecider('race_random', 'race_class');
+  on_click_decider('race_random', 'race_class');
 });
 document.getElementById("background_random").addEventListener('click', function(){
-  onClickDecider('background_random', 'background_class');
+  on_click_decider('background_random', 'background_class');
 });
 document.getElementById("class_random").addEventListener('click', function(){
-  onClickDecider('class_random', 'class_class');
+  on_click_decider('class_random', 'class_class');
 });
 document.getElementById("alignment_random").addEventListener('click', function(){
-  onClickDecider('alignment_random', 'alignment_class');
+  on_click_decider('alignment_random', 'alignment_class');
 });
 
 // Section to initially format all dropdowns
-formatInitialDropdowns('race_list', 'race_dropdown');
-formatInitialDropdowns('background_list', 'background_dropdown');
-formatInitialDropdowns('class_list', 'class_dropdown', 'class_class');
-formatInitialDropdowns('alignment_list', 'alignment_dropdown');
+format_initial_dropdowns('race_list', 'race_dropdown');
+format_initial_dropdowns('background_list', 'background_dropdown');
+format_initial_dropdowns('class_list', 'class_dropdown', 'class_class');
+format_initial_dropdowns('alignment_list', 'alignment_dropdown');
 
 // Section to format the secondary dropdowns
-formatSecondaryDropdowns('dragonborn_list', 'dragonborn_dropdown', 'dragonborn_checkbox');
-formatSecondaryDropdowns('dwarf_list', 'dwarf_dropdown', 'dwarf_checkbox');
-formatSecondaryDropdowns('elf_list', 'elf_dropdown', 'elf_checkbox');
-formatSecondaryDropdowns('gnome_list', 'gnome_dropdown', 'gnome_checkbox');
-formatSecondaryDropdowns('halfling_list', 'halfling_dropdown', 'halfling_checkbox');
-formatSecondaryDropdowns('human_list', 'human_dropdown', 'human_checkbox');
+format_secondary_dropdowns('dragonborn_list', 'dragonborn_dropdown', 'dragonborn_checkbox');
+format_secondary_dropdowns('dwarf_list', 'dwarf_dropdown', 'dwarf_checkbox');
+format_secondary_dropdowns('elf_list', 'elf_dropdown', 'elf_checkbox');
+format_secondary_dropdowns('gnome_list', 'gnome_dropdown', 'gnome_checkbox');
+format_secondary_dropdowns('halfling_list', 'halfling_dropdown', 'halfling_checkbox');
+format_secondary_dropdowns('human_list', 'human_dropdown', 'human_checkbox');
 
 // Section that adds all the clicking events for all dropdowns
-addEventsForDropdowns('.alignment_dropdown_option_checker', 'alignment_random', 'alignment_class', 'click', onDropdownOptionClick);
-addEventsForDropdowns('.class_dropdown_option_checker', 'class_random', 'class_class', 'click', onDropdownOptionClick);
-addEventsForDropdowns('.background_dropdown_option_checker', 'background_random', 'background_class', 'click', onDropdownOptionClick);
-addEventsForDropdowns('.race_dropdown_option_checker', 'race_random', 'race_class', 'click', onDropdownOptionClick);
-addEventsForDropdowns('.subrace_dropdown_option_checker', 'race_random', 'race_class', 'mousedown', onSubraceDropdownOptionClick);
+add_events_for_dropdowns('.alignment_dropdown_option_checker', 'alignment_random', 'alignment_class', 'click', on_dropdown_option_click);
+add_events_for_dropdowns('.class_dropdown_option_checker', 'class_random', 'class_class', 'click', on_dropdown_option_click);
+add_events_for_dropdowns('.background_dropdown_option_checker', 'background_random', 'background_class', 'click', on_dropdown_option_click);
+add_events_for_dropdowns('.race_dropdown_option_checker', 'race_random', 'race_class', 'click', on_dropdown_option_click);
+add_events_for_dropdowns('.subrace_dropdown_option_checker', 'race_random', 'race_class', 'mousedown', on_subrace_dropdown_option_click);
